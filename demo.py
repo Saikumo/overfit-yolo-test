00ba5df7e59cb4ddb94d0e338aefdef9f979dcce
Saikumo <saikumo11@gmail.com>
2025-01-22 16:39:11 +0800

    init

--- demo.py
+++ demo.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("/kaggle/working/overfit-yolo-test/yolo11n.yaml")  # build a new model from YAML
 
 # Train the model
-results = model.train(data="/kaggle/working/overfit-yolo-test/data.yaml", epochs=1000, imgsz=640)
+results = model.train(epochs=1000, imgsz=640)
