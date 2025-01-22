24226c0f1933577fc64f753b6b53fad8b4760c1d
Saikumo <saikumo11@gmail.com>
2025-01-22 17:00:10 +0800

    init

--- demo.py
+++ demo.py
@@ -6,5 +6,5 @@ model = YOLO("yolo11n.pt")
 
 # Train the model with custom hyperparameters
 results = model.train(data="/kaggle/working/overfit-yolo-test/data.yaml",
-                      epochs=100,
+                      epochs=1000,
                       imgsz=640,augment=False)
